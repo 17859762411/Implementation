5f23a0eff4ab35ae1188d2b8733cb1ddfd9fb40c
LvTong <2293983846@qq.com>
2019-08-12 16:58:34 +0800

    新增两个工具类：常用、图片 1.常用：复制内容到剪贴板 2.图片：从view中获取bitmap、使用string生成二维码返回bitmap、保存bitmap到相册

---
+++ implementationlibary/src/main/java/com/android/lvtong/implementationlibary/ImgUtils.java
@@ -0,0 +1,115 @@
+package com.android.lvtong.implementationlibary;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Hashtable;
+
+/**
+ * @author 22939 图片工具类
+ */
+public class ImgUtils {
+
+    public static Bitmap getBitmap(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Drawable bgDrawable = view.getBackground();
+        bgDrawable.draw(canvas);
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    public static Bitmap createImage(String url, int w, int h) {
+
+        try {
+            //判断URL合法性
+            if (url == null || "".equals(url) || url.length() < 1) {
+                return null;
+            }
+            Hashtable<EncodeHintType, String> hints = new Hashtable<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            //图像数据转换，使用了矩阵转换
+            BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, w, h, hints);
+            int[] pixels = new int[w * h];
+            //下面这里按照二维码的算法，逐个生成二维码的图片，
+            //两个for循环是图片横列扫描的结果
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * w + x] = 0xff000000;
+                    } else {
+                        pixels[y * w + x] = 0xffffffff;
+                    }
+                }
+            }
+            //生成二维码图片的格式，使用ARGB_8888
+
+            Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+            return bitmap;
+        } catch (WriterException e) {
+            Log.d("QRcodeActivity", "e:" + e);
+            return null;
+        }
+    }
+
+    public static Boolean saveImageToGallery(Context context, Bitmap bmp) {
+
+        //首先保存图片 创建文件夹
+        File appDir = new File(Environment.getExternalStorageDirectory(), "zxingdemo");
+        if (!appDir.exists()) {
+            if (!appDir.mkdir()) {
+                Toast.makeText(context, "创建目录失败", Toast.LENGTH_SHORT)
+                     .show();
+                return false;
+            }
+        }
+        //图片文件名称
+        String fileName = "qr_" + System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            Log.d("ImgUtils", "e:" + e);
+            return false;
+        }
+
+        // 其次把文件插入到系统图库
+        String path = file.getAbsolutePath();
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(), path, fileName, null);
+        } catch (FileNotFoundException e) {
+            Log.d("ImgUtils", "e:" + e);
+            return false;
+        }
+        // 最后通知图库更新
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        context.sendBroadcast(intent);
+        Toast.makeText(context, "保存成功", Toast.LENGTH_SHORT)
+             .show();
+        return true;
+    }
+}
